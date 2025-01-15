4025088449be61489243d57523fba99d75e1450a
hpreksha <preksha.hirani@marutitech.com>
2025-01-15 15:51:09 +0530

    commit2

--- medical-record-management/src/app/app.routes.ts
+++ medical-record-management/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { OtpComponent } from './components/otp/otp.component';
 
 export const routes: Routes = [
 
-    {path:'', component: LoginComponent}
+    {path:'', component: LoginComponent, pathMatch:'full'},
+    {path:'otp', component: OtpComponent, pathMatch:'full'}
 ];
