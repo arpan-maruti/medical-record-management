80fc79df52b146d9c0542f8cffbfc0d6a663840e
hpreksha <preksha.hirani@marutitech.com>
2025-01-17 11:24:38 +0530

    feat:Added basic validation for otp component.

--- medical-record-management/src/app/components/otp/otp.component.ts
+++ medical-record-management/src/app/components/otp/otp.component.ts
@@ -1,15 +1,38 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-otp',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './otp.component.html',
-  styleUrl: './otp.component.css'
+  styleUrl: './otp.component.css',
 })
 export class OtpComponent {
   constructor(private router: Router) {}
   login() {
-  this.router.navigate(['/']);
-}
-
+    this.router.navigate(['/']);
+  }
+  otp: string = '';
+  inputType: string = 'password';
+  otpError: boolean = false;
+  toggleVisibility(): void {
+    this.inputType = 'text';
+    console.log(this.otp);
+    setTimeout(() => {
+      this.inputType = 'password';
+    }, 1000); // 2000 milliseconds = 2 seconds
+  }
+  verifyOtp(otpInput:any): void {
+    // Check if the OTP is empty or incorrect
+    if (otpInput.invalid) {
+      return; // Don't proceed if input is invalid (empty)
+    }
+    if (this.otp !== '1234') {
+      this.otpError = true; // Show error if OTP is incorrect
+    } else {
+      this.otpError = false;
+      alert('OTP Verified!');
+    }
+  }
 }
