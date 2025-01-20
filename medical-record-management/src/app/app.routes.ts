e36ac8ad8003d808ec0ded494e8ab2900e116a56
hpreksha <preksha.hirani@marutitech.com>
2025-01-20 17:54:28 +0530

    feat:Added page-not-found component.

--- medical-record-management/src/app/app.routes.ts
+++ medical-record-management/src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { OtpComponent } from './components/otp/otp.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
 
     {path:'', component: LoginComponent, pathMatch:'full'},
-    {path:'otp', component: OtpComponent, pathMatch:'full'}
+    {path:'otp', component: OtpComponent, pathMatch:'full'},
+    {path:'page-not-found', component: PageNotFoundComponent, pathMatch:'full'},
+    {path:'**', redirectTo:'/page-not-found'}
 ];
