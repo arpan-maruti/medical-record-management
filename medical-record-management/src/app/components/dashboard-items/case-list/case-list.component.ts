7b049b1cc85b1c054bb246136d6a4bf7c681145c
hpreksha <preksha.hirani@marutitech.com>
2025-01-23 14:22:36 +0530

    feat:Updated no-case screen with bootsrap.

--- medical-record-management/src/app/components/dashboard-items/case-list/case-list.component.ts
+++ medical-record-management/src/app/components/dashboard-items/case-list/case-list.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, ChangeDetectorRef } from '@angular/core';
+import dataset from '../../../../assets/data.json';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-case-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './case-list.component.html',
   styleUrl: './case-list.component.css'
 })
 export class CaseListComponent {
+  data: any;
+  isDataAvailable: boolean = false;
+  loiTypes: any[] = [];
+  instructionType: any[] = [];
+  selectedLoiType: any[] = [];
+  constructor(private cdr: ChangeDetectorRef) {}
+  ngAfterViewInit() {
+    this.data;
+    if(this.data) {
+    console.log(this.data);
+    this.isDataAvailable = true;
+    }
+    this.cdr.detectChanges();
+  }
 
 }
