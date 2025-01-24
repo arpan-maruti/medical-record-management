88786e1f1a67fb335b0885a3d96831bbb78e75b8
hpreksha <preksha.hirani@marutitech.com>
2025-01-24 15:55:31 +0530

    style:Updated heading format for profile.

--- medical-record-management/src/app/components/dashboard-items/main-layout/main-layout.component.ts
+++ medical-record-management/src/app/components/dashboard-items/main-layout/main-layout.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
-import { CaseListComponent } from '../case-list/case-list.component';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [RouterOutlet],
+  imports : [RouterOutlet, CommonModule],
   templateUrl: './main-layout.component.html',
-  styleUrl: './main-layout.component.css'
+  styleUrls: ['./main-layout.component.css']  // Corrected to styleUrls (plural)
 })
 export class MainLayoutComponent {
+  fullUrl: string = '';
+  isProfile: boolean = false;
 
+  constructor(private router: Router) {}
+
+  ngDoCheck(): void {
+    // Get the full URL (including path, query params, fragment)
+    this.fullUrl = this.router.url;
+    console.log(this.fullUrl);  // Logs the full URL
+
+    // Checking if URL contains 'profile'
+    if (this.fullUrl.includes('profile')) {
+      this.isProfile = true;  // Sets isProfile to true if 'profile' is in the URL
+    }
+  }
 }
