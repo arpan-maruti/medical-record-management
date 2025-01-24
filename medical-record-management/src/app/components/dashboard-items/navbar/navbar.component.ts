b2f033bcb7167de39208a297758bddcf4e9626ee
hpreksha <preksha.hirani@marutitech.com>
2025-01-24 14:28:23 +0530

    style:Made profile responsive.

--- medical-record-management/src/app/components/dashboard-items/navbar/navbar.component.ts
+++ medical-record-management/src/app/components/dashboard-items/navbar/navbar.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule, NgClass } from '@angular/common';
 import { Component, HostListener } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { Router } from '@angular/router';
 import { ChangeDetectorRef } from '@angular/core';
 import { DataService } from '../../../data.service';
 @Component({
@@ -13,7 +13,6 @@ import { DataService } from '../../../data.service';
 export class NavbarComponent {
 
   isDropdownOpen = false;
-
   // Toggle the dropdown visibility
   toggleDropdown(event: MouseEvent) {
     this.isDropdownOpen = !this.isDropdownOpen;
@@ -25,9 +24,15 @@ export class NavbarComponent {
     this.isDropdownOpen = false; // Close dropdown on resize (optional)
   }
   user: any;
-  constructor(private dataService: DataService, private cdr: ChangeDetectorRef) {}
+  constructor(private dataService: DataService, private cdr: ChangeDetectorRef, private router: Router) {}
   ngAfterViewInit() : void {
     this.user=this.dataService.getUsers()[0];
     this.cdr.detectChanges();
+  }
+  toProfile() {
+    this.router.navigate(['/dashboard/profile']);
+  }
+  toLogout() {
+    this.router.navigate(['/']);
   }
 }
