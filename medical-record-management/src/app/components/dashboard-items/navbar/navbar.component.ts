86b32515f98b0bdf985ccd987e0d5fd90b4b6cff
arpan-maruti <arpan.rupareliya@marutitech.com>
2025-01-24 10:26:42 +0530

    chore: minor changes

--- medical-record-management/src/app/components/dashboard-items/navbar/navbar.component.ts
+++ medical-record-management/src/app/components/dashboard-items/navbar/navbar.component.ts
@@ -1,30 +1,29 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgClass } from '@angular/common';
 import { Component, HostListener } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-navbar',
-  imports: [FormsModule,CommonModule],
+  imports: [FormsModule,CommonModule,NgClass],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
 
-  isDropdownOpen: boolean = false; // To track dropdown state
+  isDropdownOpen = false;
 
-  ngOnInit(): void {
-    // Optional: Initial setup if needed
-  }
-
-  // Method to toggle the dropdown visibility
-  toggleDropdown(): void {
+  // Toggle the dropdown visibility
+  toggleDropdown(event: MouseEvent) {
     this.isDropdownOpen = !this.isDropdownOpen;
+    console.log(this.isDropdownOpen);
+    event.stopPropagation(); // Prevent the click from propagating to the document
   }
 
-  // Close the dropdown if the window is resized (optional for better UI experience)
-  @HostListener('window:resize', ['$event'])
-  onResize(): void {
-    this.isDropdownOpen = false; // Close dropdown on resize (optional)
+  // Close the dropdown when clicking outside
+  @HostListener('document:click', ['$event'])
+  closeDropdown(event: MouseEvent) {
+    if (event.target && !(event.target as Element).closest('.profile-box')) {
+      this.isDropdownOpen = false;
+    }
   }
-
 }
