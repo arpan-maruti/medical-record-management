2ed6765351dc0b17d586a6214bdaf97c1d7e4e52
hpreksha <preksha.hirani@marutitech.com>
2025-01-27 11:12:07 +0530

    style: updates in sidebar, and main-layout.

--- medical-record-management/src/app/path-format.pipe.ts
+++ medical-record-management/src/app/path-format.pipe.ts
@@ -6,14 +6,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class PathFormatPipe implements PipeTransform {
 
   transform(value: string): string {
-    if (!value) return value;  // Return empty string if value is falsy
+    if (!value) return value;
 
-    // Replace slashes, hyphens, and capitalize the first letter of each word
+    // Replace slashes, hyphens, and capitalize the first letter of each word and remove first slash and space.
     return value
       .replace(/[-/]/g, ' ')  // Replace hyphen and slash with space
       .split(' ')  // Split by spaces
       .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())  // Capitalize the first letter of each word
       .join(' / ').slice(2);  // Join the words back together with spaces
   }
-
 }
