8736e6408544a844fb70b786e87cb55757baa367
arpan-maruti <arpan.rupareliya@marutitech.com>
2025-02-04 15:57:05 +0530

    feat: swagger file added

---
+++ src/app/components/swagger-viewer/swagger-viewer.component.ts
@@ -0,0 +1,28 @@
+import { Component, AfterViewInit, ElementRef, Inject, PLATFORM_ID, ViewEncapsulation } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import SwaggerUI from 'swagger-ui';
+import "swagger-ui-dist/swagger-ui.css";
+
+@Component({
+  selector: 'app-swagger-viewer',
+  templateUrl: './swagger-viewer.component.html',
+  styleUrls: ['./swagger-viewer.component.css'],
+  encapsulation: ViewEncapsulation.None // Disable view encapsulation
+})
+export class SwaggerViewerComponent implements AfterViewInit {
+  
+  constructor(
+    private el: ElementRef,
+    @Inject(PLATFORM_ID) private platformId: object // Detects the platform (browser/server)
+  ) {}
+
+  ngAfterViewInit(): void {
+    // Run Swagger UI only in the browser
+    if (isPlatformBrowser(this.platformId)) {
+      SwaggerUI({
+        domNode: this.el.nativeElement.querySelector('#swagger-container'),
+        url: 'assets/openapi.json',
+      });
+    }
+  }
+}
